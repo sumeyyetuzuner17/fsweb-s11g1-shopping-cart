d206e238d9a0a8d7230488a723fd52c1d0a393d9
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-11 11:49:51 +0300

    5-6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import { data } from "./data";
 import { ProductContext } from "./context/ProductContext";
 // Bileşenler
+import { CartContext } from "./context/CartContext";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
@@ -19,6 +20,8 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{products,addItem}}>
+        <CartContext.Provider value={{cart}}/>
+        
         <Navigation cart={cart} />
 
         {/* Routelar */}
@@ -31,6 +34,7 @@ function App() {
             <ShoppingCart cart={cart} />
           </Route>
         </main>
+        <CartContext.Provider />
       </ProductContext.Provider>
     </div>
   );
